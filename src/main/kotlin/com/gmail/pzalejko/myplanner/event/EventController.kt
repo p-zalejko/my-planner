5d2505cb043e590e30deb658ffa4f06875887882
Pawel Zalejko <p.zalejko@gmail.com>
2019-08-20 23:17:52 +0200

    added a reactive endpoint

--- src/main/kotlin/com/gmail/pzalejko/myplanner/event/EventController.kt
+++ src/main/kotlin/com/gmail/pzalejko/myplanner/event/EventController.kt
@@ -1,15 +1,26 @@
-package com.gmail.pzalejko.myplanner;
+package com.gmail.pzalejko.myplanner.event;
 
+import io.vertx.axle.core.Vertx
+import org.reactivestreams.Publisher
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
+
 @Path("/events")
-class EventController{
+class EventController {
+
+    @Inject
+    lateinit var vertx: Vertx
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    fun getAll() = "foo bar"
-
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    fun getAll(): Publisher<String> {
+        val toPublisherBuilder = vertx.periodicStream(200).toPublisherBuilder()
+        return toPublisherBuilder
+                .map { "hello" }
+                .buildRs()
+    }
 }
