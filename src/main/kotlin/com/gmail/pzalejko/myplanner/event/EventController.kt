6b974978c214dadd089fdcb17cbe673bf84289db
Pawel Zalejko <p.zalejko@gmail.com>
2019-10-11 23:06:50 +0200

    small fixes

--- src/main/kotlin/com/gmail/pzalejko/myplanner/event/EventController.kt
+++ src/main/kotlin/com/gmail/pzalejko/myplanner/event/EventController.kt
@@ -2,11 +2,11 @@ package com.gmail.pzalejko.myplanner.event;
 
 import com.mongodb.client.MongoClient
 import io.vertx.axle.core.Vertx
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 import javax.inject.Inject
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/events")
 @Produces(MediaType.APPLICATION_JSON)
@@ -28,10 +28,12 @@ class EventController {
     }
 
     @POST
-    fun addNew(name: EventDto): CompletionStage<String> {
+    fun addNew(name: EventDto): CompletionStage<Response> {
         val event = Event(name = name.name)
         val addNew = service.addNew(event)
-        return addNew.thenCompose { d -> CompletableFuture.completedFuture("OK") }
+        return addNew
+                .thenApply(Response::ok)
+                .thenApply(Response.ResponseBuilder::build);
     }
 
     class EventDto {
