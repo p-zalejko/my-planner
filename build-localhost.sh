0441899272eaee7c50a4d20f210804d2e1f513c4
Pawel Zalejko <p.zalejko@gmail.com>
2019-11-19 22:34:29 +0100

    fixed dockerfile build

--- build-localhost.sh
+++ build-localhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn package -Pnative -Dquarkus.native.container-runtime=podman
+mvn clean package -Pnative -Dquarkus.native.container-runtime=podman
 podman build -f docker/Dockerfile.native -t pzalejko/my-planner .
 podman-compose -f docker/docker-compose.yml up
