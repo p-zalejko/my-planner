7073516e69dd51d6eb9ec571d7fcd0571a7c57e5
Pawel Zalejko <p.zalejko@gmail.com>
2019-11-20 22:51:56 +0100

    added jaeger

--- build-localhost.sh
+++ build-localhost.sh
@@ -2,4 +2,4 @@
 
 mvn clean package -Pnative -Dquarkus.native.container-runtime=podman
 podman build -f docker/Dockerfile.native -t pzalejko/my-planner .
-podman-compose -f docker/docker-compose.yml up
+podman-compose -f docker/docker-compose-all.yml up
--- calendar/calendar-client-google/src/main/kotlin/com/gmail/pzalejko/myplanner/calendar/client/google/GoogleCalendarEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.gmail.pzalejko.myplanner.calendar.client.google
-
-import com.gmail.pzalejko.myplanner.calendar.Event
-
-class GoogleCalendarEvent(val event: com.google.api.services.calendar.model.Event) : Event {
-}
