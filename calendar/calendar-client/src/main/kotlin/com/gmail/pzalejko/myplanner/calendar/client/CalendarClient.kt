0b608d6e81ec4a073f4df3f256e10fefd72f5b8a
Pawel Zalejko <p.zalejko@gmail.com>
2019-11-20 23:25:26 +0100

    updated API

--- calendar/calendar-client/src/main/kotlin/com/gmail/pzalejko/myplanner/calendar/client/CalendarClient.kt
+++ calendar/calendar-client/src/main/kotlin/com/gmail/pzalejko/myplanner/calendar/client/CalendarClient.kt
@@ -1,8 +1,13 @@
 package com.gmail.pzalejko.myplanner.calendar.client
 
 import com.gmail.pzalejko.myplanner.calendar.Event
+import java.time.Instant
+import java.util.concurrent.Flow
 
+/**
+ * Provides access to the events stored in calendar-oriented manner.
+ */
 interface CalendarClient {
 
-    fun getEvents(): List<Event>
+    fun getEvents(from: Instant, to: Instant): Flow.Publisher<Event>
 }
--- calendar/calendar-model/src/main/kotlin/com/gmail/pzalejko/myplanner/calendar/Event.kt
+++ calendar/calendar-model/src/main/kotlin/com/gmail/pzalejko/myplanner/calendar/Event.kt
@@ -1,4 +1,19 @@
 package com.gmail.pzalejko.myplanner.calendar
 
+import java.time.Instant
+
+/**
+ * An event, appointment etc.
+ */
 interface Event {
+
+    fun getId(): String
+
+    fun getStartDate(): Instant
+
+    fun getEndDate(): Instant
+
+    fun getTitle(): String
+
+    fun getDescription(): String
 }
